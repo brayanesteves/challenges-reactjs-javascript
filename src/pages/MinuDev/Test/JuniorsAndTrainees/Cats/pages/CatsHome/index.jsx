61403df39f96c971b33b2609c02e456760469ff5
Brayan Esteves <brayan.esteves93@gmail.com>
2023-09-25 20:27:02 -0400

    Test MinuDevs - Handle Error [25/09/2023]

--- src/pages/MinuDev/Test/JuniorsAndTrainees/Cats/pages/CatsHome/index.jsx
+++ src/pages/MinuDev/Test/JuniorsAndTrainees/Cats/pages/CatsHome/index.jsx
@@ -6,9 +6,11 @@ const CAT_PREFIX_IMAGE_URL     = 'https://cataas.com/';
 
 export const CatsHome = () => { 
     const [fact, setFact]                 = useState('lorem ipsum cat fact whatever');   
+    const [factError, setFactError]       = useState();
     const [imageUrl, setImageUrl]         = useState();
     const [asynchronous, setAsynchronous] = useState(false);
 
+    // To retrieve the quote on page load.
     useEffect(() => {
         if(asynchronous) {
             async function getRandomFact() {
@@ -18,20 +20,34 @@ export const CatsHome = () => {
             }
             getRandomFact();
         } else {
-            fetch(CAT_ENDPOINT_RANDOM_FACT).then(res => res.json()).then(data => {
+            fetch(CAT_ENDPOINT_RANDOM_FACT).then(res => {
+                // TODO: Handle eror if '!res.ok'.
+                if(!res.ok) {
+                    // setFactError("The appointment could not be recovered.");
+                    throw new Error('Error fetching "fact".');
+                }
+                return res.json();
+            }).then(data => {
                 const { fact } = data;
-                setFact(fact);
-
-                const threeFirstWord = fact.split(' ').slice(0, 3).join(' ');
-                // console.log(firstWord);
-                fetch(`https://cataas.com/cat/says/${threeFirstWord}?size=50&color=red&json=true`).then(res => res.json()).then(response => {
-                    // console.log(response);
-                    const { url } = response;
-                    setImageUrl(url);
-                });
+                setFact(fact);                
+            }).catch((error) => {
+                // Whether there is an error with the answer.
+                // How if there is an error with the request.
             });
         }
     }, []);
+
+    // To recover the image every time we have a new appointment.
+    useEffect(() => {        
+        if(!fact) return;
+        const threeFirstWord = fact.split(' ').slice(0, 3).join(' ');
+        // console.log(firstWord);
+        fetch(`https://cataas.com/cat/says/${threeFirstWord}?size=50&color=red&json=true`).then(res => res.json()).then(response => {
+            // console.log(response);
+            const { url } = response;
+            setImageUrl(url);
+        });
+    }, [fact]);
     
     return (
         <main>
